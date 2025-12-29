d31d471b2c016ecc873f0404d1f8eb682c0563ac
Saksham <guptasaksham795@gmail.com>
2025-12-29 17:13:22 +0530

    updated

--- frontend/src/components/Main/Main.jsx
+++ frontend/src/components/Main/Main.jsx
@@ -98,7 +98,7 @@ const Main = () => {
             <div>
               <img src={assets.gallery_icon} alt="" />
               <img src={assets.mic_icon} alt="" />
-              <img src={assets.send_icon} alt="" onClick={onSent}/>
+              {input?<img src={assets.send_icon} alt="" onClick={onSent}/>:null}
             </div>
           </div>
 
