4337d734c506c2f2711804600d4212504a7bf1c9
Saksham <guptasaksham795@gmail.com>
2025-12-30 16:39:32 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
+import './App.css'
 
 const App = () => {
   return (
-    <>
-    <Sidebar />
-    <Main />
-    </>
+    <div className="app">
+      <Sidebar />
+      <Main />
+    </div>
   )
 }
 
