28b7c94376c0a3fd7eb9c3bd19963723a5df00e2
Saksham <guptasaksham795@gmail.com>
2025-12-30 16:44:12 +0530

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
-import './App.css'
 
 const App = () => {
   return (
+    <>
+     <style>{`
+        .app {
+          display: flex;
+          width: 100vw;
+          height: 100vh;
+          overflow: hidden;
+        }
+      `}</style>
+
     <div className="app">
       <Sidebar />
       <Main />
     </div>
+    </>
   )
 }
 
